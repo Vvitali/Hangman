c4bd637f51c2156db521e9347a8a008a1481d1ce
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-02 03:34:52 +0000

    the first working prototype (0.3)

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var theWord = document.getElementById("theword");
 var remainGuesses = document.getElementById("guessesRemain");
-var wordlist = ["word", "mother", "dog", "Honorificabilitudinitatibus", "Aluminium "];
-
-
+var wordlist = ["word", "mother", "dog", "English", "aluminium", "music", "advertisement"];
 
 var gameM = {
 
     guessedList: [],
-    underscorePlaceHolder: "",
+    underscorePlaceHolder: [],
     secretWord: undefined,
 
     generateWord: function() {
@@ -16,47 +14,77 @@ var gameM = {
     },
 
     onInput: function(event) {
-        console.log(event.key.toLowerCase());
+        var thePressedKey = event.key.toLowerCase();
+        console.log("Pressed:" + thePressedKey);
+        //if statement - only for debuging
+        if (this.checkTheList(thePressedKey)) { console.log(thePressedKey + " letter already  does exist in the list") };
+
+        if (this.checkTheWord(thePressedKey)) {
+            this.indexesOfLetter(thePressedKey).forEach(function(element, index) {
+                console.log("The letter in the word!")
+                this.gameM.updateUnderScorePlaceHolder(element, thePressedKey);
+            })
+        }
     },
 
     checkTheList: function(theKey) {
         if (this.guessedList.indexOf(theKey) == -1) {
             this.guessedList.push(theKey);
-            //if key does not existed in the list - we push the key to the array, and function returns 1
+            //if letter does not existed in the list - we push the key to the array, and function returns 0
             return 0;
         }
-        //if key does exist in the list - just return 0
+        //if letter does exist in the list - just return 1
         return 1;
     },
 
     checkTheWord: function(theKey) {
-        if (theWord.indexOf(theKey) == -1) {
+        if (this.secretWord.indexOf(theKey) == -1) {
+            //if letter does not existed in the word - function returns 0
             return 0;
         }
         else { return 1; }
     },
 
     initializeHtmlByUnderscore: function(length) {
-        var underscores = "";
+        var underscores = [];
         for (var i = 0; i < length; i++) {
-            underscores += "_ ";
+            underscores[i] = "_";
         }
-        theWord.innerHTML = underscores;
         this.underscorePlaceHolder = underscores;
+        underscores = underscores.join(" ");
+
+        theWord.innerHTML = underscores;
     },
 
     updateUnderScorePlaceHolder: function(position, letter) {
+        //console output
+        console.log(position);
         console.log("Holder: " + this.underscorePlaceHolder);
+        //update the array for internal needs
         this.underscorePlaceHolder[position] = letter;
-        console.log("Holder: " + this.underscorePlaceHolder);
-        console.log("New letter " + letter + ", on the position#" + position);
-        theWord.innerHTML = this.underscorePlaceHolder;
+        //console outup
+        console.log("New letter " + letter + ", on the position #" + position);
+        //update html element
+        theWord.innerHTML = this.underscorePlaceHolder.join(" ");
+    },
+    //Calculate how many times the letter excists in the word
+    indexesOfLetter: function(letter) {
+        var indexes = [];
+
+        for (var i = 0; i < this.secretWord.length; i++)
+            if (this.secretWord[i] === letter) indexes.push(i);
+        return indexes;
     }
 
 }
-//generate the word
+//1) generate the word
 this.gameM.generateWord();
-//put a underscore-placeholder to the html
+//2) put a underscore-placeholder to the html
 this.gameM.initializeHtmlByUnderscore(gameM.secretWord.length);
+
+//check if the letter exists in the word - this stage is running only when user pressed any key (Check function onInput())
+
+
+
 //update placeholder with new-letter
-this.gameM.updateUnderScorePlaceHolder(2, "k");
+//
